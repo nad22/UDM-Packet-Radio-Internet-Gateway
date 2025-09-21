3290320b09dc916be57878c6f878cadb49c2669d
nad22 <nad@pukepals.com>
2025-09-21 12:12:46 +0200

    Removed Debug Files

--- decode_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$data = "wABmbSBQUkdTUlYgdG8gVURNIGN0bCBVSV4gcGlkIEYwRklOQUxfVEVTVF8yMDo1OMA=";
-$decoded = base64_decode($data);
-echo "Decoded data (" . strlen($decoded) . " bytes):\n";
-echo $decoded . "\n";
-?>
